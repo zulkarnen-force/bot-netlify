9f829bd855ad2fd053670ef6acb00c7b652136a5
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2023-03-23 21:19:58 +0700

    oke apapaun itu

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
-    let bot = Telegraf("6150107976:AAEha3FUSQFDDdNpkUH4JMBIiy3rqzvHzYA");
+    let bot = new Telegraf("6150107976:AAEha3FUSQFDDdNpkUH4JMBIiy3rqzvHzYA");
     bot.on(message('text'), (ctx) => {
         ctx.reply('ok apapapun itu')
     })
