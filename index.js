4d16052acad7f7f497c342edea50aa2bccbd450b
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2023-03-23 21:04:37 +0700

    add express js

---
+++ index.js
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import express from "express";
+const port = 3000
+let app = express();
+let router = express.Router()
+
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    return res.json(req.body);
+})
+
+app.listen(port, () => {
+    console.log('server running')
+})
