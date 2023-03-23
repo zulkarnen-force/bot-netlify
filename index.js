e1e3cbe4d92bd107e7ec0feb8ee801c1f809e1d8
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2023-03-23 21:13:29 +0700

    add webhooks

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
+    console.log(req.json);
+    console.log('post called');
     return res.json(req.body);
 })
 
 app.get('/', (req, res) => {
+    
     return res.json('ok');
 })
 
