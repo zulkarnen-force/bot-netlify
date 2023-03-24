07e19cd56bbaff1e9ddd51afd4f4ecb9a5f7d251
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2023-03-24 19:30:34 +0700

    cleaning

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bot.webhookCallback('/secret-path'));
-bot.telegram.setWebhook('https://bot-netlify.vercel.app/secret-path');
+// app.use(bot.webhookCallback('/secret-path'));
+// bot.telegram.setWebhook('https://bot-netlify.vercel.app/secret-path');
 
 
 // app.post('/secret-path', (req, res) => {
@@ -34,9 +34,14 @@ app.post('/', (req, res) => {
 
 app.post('/test/hook/path', (req, res) => {
     
-    bot.on(message('text'), (ctx) => {
-        return ctx.reply(`server vercel ${ctx.message.text}`)
-    })
+    try {
+        bot.on(message('text'), (ctx) => {
+            return ctx.reply(`server vercel ${ctx.message.text}`)
+        })
+    } catch (err) {
+        console.error(err.message)
+    }
+    
    
 
 })
