223c1575fdbf63091bd9753040c05a391ac538b3
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2023-03-24 22:15:35 +0700

    test

--- index.js
+++ index.js
@@ -1,78 +1,42 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { Telegraf } from "telegraf";
+import { Telegraf, Telegram } from "telegraf";
 import { message } from "telegraf/filters";
+
 const port = 3000
 let app = express();
 
 let bot = new Telegraf("6150107976:AAEha3FUSQFDDdNpkUH4JMBIiy3rqzvHzYA");
-
+let telegram = new Telegram("6150107976:AAEha3FUSQFDDdNpkUH4JMBIiy3rqzvHzYA");
+app.use(await bot.createWebhook({domain: "https://a018-2001-448a-4042-15d9-109a-b130-cbbd-d255.ap.ngrok.io/"}))
+bot.on("text", ctx => ctx.reply("Hello"));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set the bot API endpoint
-app.use(await bot.createWebhook({ domain:  "https://bot-netlify.vercel.app", path: '/test/handle'}));
-// bot.on("text", ctx => ctx.reply("Hello"));
 
-// app.use(bot.webhookCallback('/secret-path'));
-// bot.telegram.setWebhook('https://bot-netlify.vercel.app/secret-path');
-
-
-// app.post('/secret-path', (req, res) => {
-    
-//     bot.on(message('text'), (ctx) => {
-//         return ctx.reply('zulkarnen sangat ganteng')
-//     })
-
-//     bot.launch();
+// app.post('/hook/path', (req, res) => {
+//     let from = req.body.message.from
+//     let chatId = req.body.message.chat.id;
+//     let message = req.body.message.text;
+//     telegram.sendMessage(chatId, message);
 // })
 
-
-// app.post('/', (req, res) => {
-   
-
+// app.get('/', (req, res) => {
+//     res.json({status: 'ok'})
 // })
 
-
-
-app.post('/test/hook/path', (req, res) => {
-    
-    try {
-        bot.on(message('text'), (ctx) => {
-            return ctx.reply(`server vercel ${ctx.message.text}`)
-        })
-        bot.launch();
-    } catch (err) {
-        console.error(err.message)
-    }
-    
-   
-
-})
-
-
-app.post('/test/handle', (req, res) => {
-    bot.on('text', (ctx) => {
-        bot.reply(`hallo form handle ${ctx.message.text}`)
-    })
-})
-
-
-app.get('/', (req, res) => {
-    
-})
-
+// bot.on('text', (ctx) => {
+//     console.log(ctx)
+// })
 
 // bot.launch({
-//     webhook: { 
-//         domain: "https://bot-netlify.vercel.app",
-//         port:8000,
-//         hookPath:"/test/hook/path"
+//     webhook: {
+//         domain: "https://a018-2001-448a-4042-15d9-109a-b130-cbbd-d255.ap.ngrok.io",
+//         hookPath: '/hook/path'
 //     }
-// }).then(res => {
-//     console.info(`The bot ${bot.botInfo.username} is running on server`);
-// }).catch(err => console.log(`bot error: ${err.message}`))
+// })
+
 
 
 app.listen(port, () => {
